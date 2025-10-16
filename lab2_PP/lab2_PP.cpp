a89d8768481f10ce7f16380e5324fce8668e4403
QueenieJK <jagodaanakurowska@gmail.com>
2025-10-16 15:03:56 +0200

    gg

--- lab2_PP/lab2_PP.cpp
+++ lab2_PP/lab2_PP.cpp
@@ -1,11 +1,115 @@
-// lab2_PP.cpp : This file contains the 'main' function. Program execution begins and ends there.
+﻿// lab2_PP.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
 #include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	//cout << "Zadanie 1.2:  " << endl << endl;
+	//for (int i = 5; i <= 7; i++) {
+		//cout << i << endl;}
+	//for (int i = 5; i < 7; i++) {
+		//cout << i << endl;}
+
+	//cout << "Zadanie 1.3:  " << endl << endl;
+	//for (int i = 0; i <= 10;i+=2) {
+		//cout << i << endl;}
+	//cout << "Zadanie 1.4:  " << endl << endl;
+	//for (int i = 10; i >= 0; i--) {
+		//cout << i << endl;}
+	//cout << "Zadanie 2: " << endl << endl;
+	//int fahr,celsius;
+	//int start,limit,krok;
+	//start = 0; 
+	//limit = 200; 
+	//krok = 20; 
+	//fahr = start; 
+	//while (fahr <= limit) {
+		//celsius = 5 * (fahr - 32) / 9;
+		//cout << fahr << "\t" << celsius << endl;
+		//fahr = fahr + krok;}
+	//cout << "Zadanie 3: " << endl << endl;
+	//int fahr,celsius;
+	//int start, limit,krok;
+	//start = 0; 
+	//limit = 200; 
+	//krok = 20; 
+	//fahr = start;
+	//while (fahr <= limit){
+		//float celsius = (5.0 / 9.0) * (fahr - 32.0);
+		//cout << fahr << "\t " << celsius << endl;
+		//fahr = fahr + krok;}
+	//cout << "Zadanie 6: " << endl << endl;
+	//int fahr, celsius;
+	//int start, limit,krok;
+	//start = 0; 
+	//cout << "Podaj limit: " << endl;
+	//cin >> limit;
+	//cout << "Podaj krok: " << endl;
+	//cin >> krok;
+	//fahr = start;
+	//while (fahr <= limit){
+		//float celsius = (5.0 / 9.0) * (fahr - 32.0);
+		//cout << fahr << "\t " << celsius << endl;
+		//fahr = fahr + krok;}
+	//cout << "Zadanie 7: " << endl << endl;
+	//int liczba;
+	//for (int i = 0; i < 10; i++) {
+		//cout << "Podaj liczbe: " << endl;
+		//cin >> liczba;
+		//if (liczba > 0) {
+			//cout << liczba << endl;}
+		//else {
+			//continue;}}
+	//cout << "Zadanie 8: " << endl << endl;
+	//for (int i = 0; i <= 100; i += 2) {
+		//cout << i << endl;}
+	//cout << "Zadanie 9: " << endl << endl;
+	//int n;
+	//cout << "Podaj n: " << endl;
+	//cin >> n;
+	//for (int i = 0; i <= 100; i++) {
+		//if (i % n == 0) {
+			//cout << i << endl;}}
+	//cout << "Zadanie 10: " << endl << endl;
+	//int n;
+	//cout << "Podaj n: " << endl;
+	//cin >> n;
+	//int suma = 0;
+	//for (int i = 1; i <= 100; i++) {
+		//if (i % n == 0) {
+			//suma += 1;}}
+	//cout << suma << endl;	
+	cout << "Zadanie 11: " << endl << endl;
+	int a, b;
+	cout << " Podaj liczbe a: " << endl;
+	cin >> a;
+	cout << "Podaj liczbe b: " << endl;
+	cin >> b;
+	if (a < b) {
+		for (int i = a; i <= b; i++) {
+			if (i % 3 == 0) {
+				cout << i << endl;
+			}
+		}
+	}
+	else if (a > b) {
+		for (int i = b; i <= b; i++) {
+			if (i % 3 == 0) {
+				cout << i << endl;
+			}
+		}
+	}
+	else {
+		cout << "Liczby sa rowne" << endl;
+		if (a % 3 == 0) {
+			cout << a << endl;
+		}
+	}
+	
+
+
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
